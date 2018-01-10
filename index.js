b4f1f5f8a587ec5aed17bdce54c8a018c83d9c66
Tyler <tcameronsmith@gmail.com>
2018-01-10 16:22:46 +1100

    connect to mongoose and require user model

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+
+require('./models/User');
 require('./services/passport');
+
 const authRoutes = require('./routes/authRoutes');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
