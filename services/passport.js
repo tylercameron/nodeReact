191f736cbf2ff98720b19872ce4e3130d3892509
Tyler <tcameronsmith@gmail.com>
2018-01-10 16:23:05 +1100

    save user to user model

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,13 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+    done(null, user.id);
+});
 
 passport.use(
     new GoogleStrategy(
@@ -10,9 +17,27 @@ passport.use(
             callbackURL: '/auth/google/callback'
         },
         (accessToken, refreshToken, profile, done) => {
-            console.log('access token', accessToken);
-            console.log('refresh token', refreshToken);
-            console.log('profile:', profile);
+            // const user = await User.findOne({ googleId: profile.id });
+
+            // if (!user) {
+            //     const newUser = await new User({ googleId: profile.id }).save();
+            //     done(null, newUser);
+            // };
+
+            // done(null, user);
+
+            User.findOne({ googleId: profile.id }) // same as async await above
+                .then(existingUser => {
+                    if (existingUser) {
+                        // don't save to db
+                        done(null, existingUser); 
+                    } else {
+                        // save user to db
+                        new User({ googleId: profile.id })
+                            .save()
+                            .then(user => done(null, user));
+                    }
+                });
         }
     )
 );
