dc2f9a48ec466e6a58104235b0daaae7a7a3ad97
Tyler <tcameronsmith@gmail.com>
2018-01-10 18:12:58 +1100

    add cookie parser and initialize passport

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
 const keys = require('./config/keys');
 
 require('./models/User');
@@ -10,6 +12,16 @@ const authRoutes = require('./routes/authRoutes');
 mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(
+    cookieSession({
+        maxAge: 2592000000,
+        keys: [keys.cookieKey]
+    })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 authRoutes(app);
 
