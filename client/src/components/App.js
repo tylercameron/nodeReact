d6f9055dff497383523fb69bdef5d4d8086d0a6f
Tyler <tcameronsmith@gmail.com>
2018-03-04 16:18:10 +1100

    setup router for basic routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -10,8 +10,10 @@ const App = () => (
     <div>
         <BrowserRouter>
             <div>
-                <Route path="/" component={Landing} />
-
+                <Header />
+                <Route exact path="/" component={Landing} />
+                <Route exact path="/surveys" component={Dashboard} />
+                <Route path="/surveys/new" component={SurveyNew} />
             </div>
         </BrowserRouter>
     </div>
