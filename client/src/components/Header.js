336c0f32ec2339d89990e99bc3d946e47d12ee67
Tyler <tcameronsmith@gmail.com>
2018-03-05 17:24:07 +1100

    add links to logo and add landing component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
     renderContent() {
@@ -17,7 +18,7 @@ class Header extends Component {
         return (
             <nav>
                 <div className="nav-wrapper">
-                    <a href="" className="left brand-logo">Logo</a>
+                    <Link to={this.props.auth ? '/surveys' : '/'} className="left brand-logo">Logo</Link>
                     <ul className="right">
                         {this.renderContent()}
                     </ul>                    
---
+++ client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Landing = () => {
+    return (
+        <div style={{ textAlign: 'center' }}>
+            <h1>Emaily App</h1>
+            <p>Collectt some data from users</p>
+        </div>
+    );
+}
+
+export default Landing;
